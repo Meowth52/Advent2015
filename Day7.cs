33e6e8e736dd8176ceb87055a4fb2bb1d3040863
Meowth52 <meowth@meowth.se>
2018-03-15 11:08:06 +0100

    sdf

--- Day7.cs
+++ Day7.cs
@@ -32,12 +32,13 @@ namespace Advent2015
             int Sum = 0;
             int Sum2 = 0;
             //setting up registers
-            Dictionary<string, int> Wires = new Dictionary<string, int>();
+            Dictionary<string, Schmenum> Wires = new Dictionary<string, Schmenum>();
             foreach(string[] Instruction in Instructions)
             {
-                Wires.Add(Instruction.Last(), 0);
+                if (!Wires.ContainsKey(Instruction.Last()))
+                    Wires.Add(Instruction.Last(), new Schmenum());
             }
-           int RemoveIndex;
+            int RemoveIndex;
             Regex MatchRegister = new Regex(@"[a-z]+");
             List<string> RegisterMatch = new List<string>();
             Regex MatchInstruction = new Regex(@"[A-Z]+");
@@ -52,7 +53,7 @@ namespace Advent2015
                     {
                         if (Instruction.Last() != s && Wires.ContainsKey(s))
                         {
-                            if (Wires[s] == 0)
+                            if (!Wires[s].IsAssigned()) // tveksam kontroll
                             {
                                 Doable = false;
                                 //markera för att det går att genomföra operationen, genomför den, breaka och ta bort den.
@@ -61,18 +62,88 @@ namespace Advent2015
                     }
                     if (Doable)
                     {
+                        char Keyword = '_';
                         RemoveIndex = Instructions.IndexOf(Instruction);
-                        switch
-                        break;
+                        foreach (string s in Instruction)
+                        {
+                            if (char.IsUpper(s[0]))
+                                Keyword = s[0];
+                        }
+                        ushort Value1;
+                        ushort Value2;
+                        
+                        string Target = Instruction.Last();
+                        switch (Keyword)
+                        {
+                            case 'A': // And
+                                Value1 = GetNumber(Instruction[0], ref Wires);
+                                Value2 = GetNumber(Instruction[2], ref Wires);
+                                Wires[Target].Assign((ushort)(Value1 & Value2));
+                                break;
+                            case 'O': //Or
+                                Value1 = GetNumber(Instruction[0], ref Wires);
+                                Value2 = GetNumber(Instruction[2], ref Wires);
+                                Wires[Target].Assign((ushort)(Value1 | Value2));
+                                break;
+                            case 'L': //Lshift
+                                Value1 = GetNumber(Instruction[0], ref Wires);
+                                Value2 = GetNumber(Instruction[2], ref Wires);
+                                Wires[Target].Assign((ushort)(Value1 << Value2));
+                                break;
+                            case 'R': //Rshift
+                                Value1 = GetNumber(Instruction[0], ref Wires);
+                                Value2 = GetNumber(Instruction[2], ref Wires);
+                                Wires[Target].Assign((ushort)(Value1 >> Value2));
+                                break;
+                            case 'N': //Not
+                                Value2 = GetNumber(Instruction[1], ref Wires);
+                                Wires[Target].Assign((ushort)(~Value2));
+                                break;
+                            default:
+                                break;
+                        }
+                    break;
                     }
 
                 }
                 Instructions.RemoveAt(RemoveIndex);
             }
+            Sum = Wires["a"].GetValue();
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             return "Del 1: " + Sum.ToString() + " och del 2: " + Sum2.ToString() + " Executed in " + ts.TotalMilliseconds.ToString() + " ms";
         }
+        public ushort GetNumber(string s, ref Dictionary<string, Schmenum> Dick)
+        {
+            ushort TheNumber = 0;
+            if (char.IsDigit(s[0]))
+                ushort.TryParse(s, out TheNumber);
+            else
+                TheNumber = Dick[s].GetValue();
+            return TheNumber;
+        }
 
+    }
+    class Schmenum
+    {
+        bool Assigned;
+        ushort Value;
+        public Schmenum()
+        {
+            Assigned = false;
+        }
+        public void Assign(ushort i)
+        {
+            Assigned = true;
+            Value = i;
+        }
+        public bool IsAssigned()
+        {
+            return Assigned;
+        }
+        public ushort GetValue()
+        {
+            return Value;
+        }
     }
 }
