7eed9e0bb0ac9d380080a5b3518a8926bb114d80
Meowth52 <meowth@meowth.se>
2018-04-09 12:08:55 +0200

    Dag 10 klar

--- Day10.cs
+++ Day10.cs
@@ -11,18 +11,44 @@ namespace Advent2015
     {
         Stopwatch stopWatch = new Stopwatch();
         private string Input;
-        private string[] Instructions;
         public Day10(string input)
         {
             stopWatch.Start();
-            Input = input.Replace("\r\n", "_");
-            Instructions = Input.Split('_');
+            Input = input.Replace("\r\n", "");
         }
         public string Result()
         {
             int Sum = 0;
             int Sum2 = 0;
-            stopWatch.Stop();
+            StringBuilder NewString = new StringBuilder();
+            char LastChar;
+            int HowManyOf;
+            for (int i = 0;i <50;i++)
+            {
+                NewString.Clear();
+                LastChar = ' ';
+                HowManyOf = 1;
+                Input += "_";
+                foreach (char c in Input)
+                {
+                    if (LastChar != ' ')
+                    {
+                        if (c == LastChar)
+                        {
+                            HowManyOf++;
+                        }
+                        else
+                        {
+                            NewString.Append(HowManyOf.ToString()+LastChar.ToString());
+                            HowManyOf = 1;
+                        }
+                    }
+                    LastChar = c;
+                }
+                Input = NewString.ToString();
+            }
+            Sum = Input.Length;
+           stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             return "Del 1: " + Sum.ToString() + " och del 2: " + Sum2.ToString() + " Executed in " + ts.TotalMilliseconds.ToString() + " ms";
         }
