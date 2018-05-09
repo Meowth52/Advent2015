1f6f33de3b9dfdcbf4d6edb32dba3f04d2c8b2ad
Meowth52 <meowth@meowth.se>
2018-05-09 15:58:58 +0200

    ops vad flummigt det blev

--- Day17.cs
+++ Day17.cs
@@ -16,15 +16,92 @@ namespace Advent2015
         {
             stopWatch.Start();
             Input = input.Replace("\r\n", "_");
-            Instructions = Input.Split('_');
+            Instructions = Input.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
         }
         public string Result()
         {
             int Sum = 0;
             int Sum2 = 0;
+            int MagicNumber = 150;
+            List<int> Containers = new List<int>();
+            List<Combination> Combinations = new List<Combination>();
+            Combinations.Add(new Combination( Instructions));
+            List<Combination> NextCombinations = new List<Combination>();
+
+            while (Combinations.Count > 0)
+            {
+                foreach(Combination C in Combinations)
+                {
+                    if (C.hasSumReached(MagicNumber))
+                    {
+                        Sum++;
+                    }
+                    else if (!C.isEmpty())
+                    {
+                        NextCombinations.Add(new Combination(C.getNextSkipCombination()));
+                        NextCombinations.Add(new Combination( C.getNextCombination()));
+                    }
+                }
+                Combinations = new List<Combination>(NextCombinations);
+                NextCombinations.Clear();
+            }
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             return "Del 1: " + Sum.ToString() + " och del 2: " + Sum2.ToString() + " Executed in " + ts.TotalMilliseconds.ToString() + " ms";
+        }
+    }
+    class Combination
+    {
+        List<int> Containers;
+        int SumSoFar = 0;
+        int ValueRemoved = 0;
+        public Combination(string[] Input)
+        {
+            int TryParseInt;
+            Containers = new List<int>();
+            foreach (string s in Input)
+            {
+                TryParseInt = 0;
+                Int32.TryParse(s, out TryParseInt);
+                if (TryParseInt != 0)
+                    Containers.Add(TryParseInt);
+            }
+        }
+        public Combination(Combination OldCombination)
+        {
+            Containers = new List<int>(OldCombination.getList());
+            SumSoFar = OldCombination.getSumSoFar();
+        }
+        public bool hasSumReached(int i)
+        {
+            return SumSoFar == i;
+        }
+        public bool isEmpty()
+        {
+            return Containers.Count == 0;
+        }
+        public bool hasThreeEntries()
+        {
+            return Containers.Count() >= 3; // Because three is the magic number
+        }
+        public List<int> getList()
+        {
+            return Containers;
+        }
+        public int getSumSoFar()
+        {
+            return SumSoFar;
+        }
+        public Combination getNextCombination()
+        {
+            SumSoFar += ValueRemoved;
+            return this;
+        }
+        public Combination getNextSkipCombination()
+        {
+            ValueRemoved = Containers[0];
+            Containers.RemoveAt(0);
+            return this;
         }
     }
 }
